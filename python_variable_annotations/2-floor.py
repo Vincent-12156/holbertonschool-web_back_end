d42d99221769c35b890249089eb0298d4e12154a
Vincent-12156 <12156@holbertonstudents.com>
2026-04-08 11:00:10 +0200

    First commit

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function
+to compute the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float."""
+    return math.floor(n)
