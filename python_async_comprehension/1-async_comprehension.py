d0d9b2e1de9d4479eb543a6fe23d5431a95c5716
Vincent-12156 <12156@holbertonstudents.com>
2026-04-08 12:36:36 +0200

    Fix import

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,9 @@
 """Module that defines a coroutine for collecting random numbers."""
 
 from typing import List
-from 0-async_generator import async_generator
+
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
