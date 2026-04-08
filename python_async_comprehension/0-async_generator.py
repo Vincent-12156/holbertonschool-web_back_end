f6339fee18022021bc3f41be9d90a715aada073b
Vincent-12156 <12156@holbertonstudents.com>
2026-04-08 12:33:02 +0200

    Fix await order

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,5 +10,5 @@ async def async_generator():
     Asynchronous generator yielding 10 random numbers with 1s delay
     """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.random() * 10
+        await asyncio.sleep(1)
