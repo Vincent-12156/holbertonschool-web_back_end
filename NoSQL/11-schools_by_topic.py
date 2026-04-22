e52e2d8e356caf19e4aa213c22aba120d3f246db
Vincent-12156 <12156@holbertonstudents.com>
2026-04-22 14:28:01 +0200

    First commit

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to find schools by topic.
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools having a specific topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
