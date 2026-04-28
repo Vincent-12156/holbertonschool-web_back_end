b1a111b806e1fde08d5dbb37983d83ae1b3fa273
Vincent-12156 <12156@holbertonstudents.com>
2026-04-28 10:51:29 +0200

    First commit

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,30 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage === undefined) {
+        throw new Error(
+          "Class extending Building must override evacuationWarningMessage"
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Sqft must be a number");
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      "Class extending Building must override evacuationWarningMessage"
+    );
+  }
+}
