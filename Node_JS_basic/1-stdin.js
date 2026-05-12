3b89ba6cb5b3b4ddab93f9793a009c6d8e016e2f
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 11:42:11 +0200

    Fix typo

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,6 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
 
 process.stdin.setEncoding("utf8");
 process.stdin.on('readable', () => {
@@ -8,6 +10,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
