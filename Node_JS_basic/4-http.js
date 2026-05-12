46d99baf03450f4a01052e34454d89c4aae162c8
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 11:58:34 +0200

    Use node:http

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,13 @@
-const http = require('http');
+const { createServer } = require('node:http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": 'text/plain' });
+const host = '127.0.0.1';
+const port = 1245;
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain' );
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.listen(port, host, () => {});
 
 module.exports = app;
