d70654eddf7dd93d0336243b2a5ba496146b8a24
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 12:01:01 +0200

    Use node:http

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,30 +1,56 @@
-const http = require("http");
-const countStudents = require("./3-read_file_async");
+const { createServer } = require('node:http');
+const fs = require('fs');
 
-const app = http.createServer((req, res) => {
-  const { url } = req;
+const database = process.argv[2];
 
-  res.writeHead(200, { "Content-Type": "text/plain" });
+const hostname = '127.0.0.1';
+const port = 1245;
 
-  if (url === "/") {
-    res.end("Hello Holberton School!");
-  } else if (url === "/students") {
-    const database = process.argv[2];
-
-    res.write("This is the list of our students\n");
-
-    countStudents(database)
-      .then(() => {
+const app = createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    new Promise((resolve, reject) => {
+      if (!database) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      fs.readFile(database, 'utf-8', (err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+          return;
+        }
+        const filterFile = data.split('\n').filter((line) => line !== '');
+        const students = filterFile.slice(1);
+        const fields = {};
+        students.forEach((element) => {
+          const field = element.split(',')[3];
+          const firstname = element.split(',')[0];
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        });
+        const lines = [];
+        lines.push(`Number of students: ${filterFile.length - 1}`);
+        Object.keys(fields).forEach((field) => {
+          lines.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        });
+        resolve(lines.join('\n'));
+      });
+    })
+      .then((result) => {
+        res.write('This is the list of our students\n');
+        res.write(result);
         res.end();
       })
-      .catch(() => {
-        res.end("Cannot load the database");
+      .catch((error) => {
+        res.end(error.message);
       });
-  } else {
-    res.end("Hello Holberton School!");
   }
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {});
 
 module.exports = app;
