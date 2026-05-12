0df6589c937889656941aad05c4f3525c9e982e3
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 12:13:04 +0200

    EsLint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const countStudents = require("./3-read_file_async");
+const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", (req, res) => {
+app.get('/students', (req, res) => {
   const database = process.argv[2];
 
-  res.setHeader("Content-Type", "text/plain");
-  res.write("This is the list of our students\n");
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
 
   countStudents(database)
     .then(() => {
       res.end();
     })
     .catch(() => {
-      res.end("Cannot load the database");
+      res.end('Cannot load the database');
     });
 });
 
