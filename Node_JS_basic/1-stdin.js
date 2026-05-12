160bf605472b2f81762ad4820f2d8641134463dd
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 11:28:21 +0200

    Use data event instead og readable

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.setEncoding("utf8");
-process.stdin.on('readable', () => {
-  const data = process.stdin.read();
-  if (data) {
-    process.stdout.write(`Your name is: ${data}`);
-  }
+process.stdin.on("data", (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on("end", () => {
