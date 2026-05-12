294a04a1e9dea1f3e3f475c0f4c9aa2aa6d42803
Vincent-12156 <12156@holbertonstudents.com>
2026-05-12 11:21:35 +0200

    Delete formating string

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,8 @@
-process.stdin.setEncoding("utf8");
-
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
+process.stdin.setEncoding("utf8");
 process.stdin.on("data", (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on("end", () => {
